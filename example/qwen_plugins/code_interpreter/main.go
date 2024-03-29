55a394f1dbb47f1c0b6ac38777c332393ba8432d
devin.yf <dyfsquall@qq.com>
2024-03-29 18:55:43 +0800

    update example case

--- example/qwen_plugins/code_interpreter/main.go
+++ example/qwen_plugins/code_interpreter/main.go
@@ -24,7 +24,8 @@ func main() {
 	}
 
 	userContent := qwen.TextContent{
-		Text: "有若干只鸡兔同在一个笼子里,从上面数,有100个头,从下面数,有334只脚。问笼中各有多少只鸡和兔?",
+		// Text: "有若干只鸡兔同在一个笼子里,从上面数,有100个头,从下面数,有334只脚。问笼中各有多少只鸡和兔?",
+		Text: "使用代码画一个y=x^2的函数图",
 	}
 
 	input := dashscopego.TextInput{
@@ -53,9 +54,9 @@ func main() {
 	}
 
 	log.Println("\nnon-stream result: ")
-	log.Printf("%+v\n", resp.ToJSONStr())
+	// log.Printf("%+v\n", resp.ToJSONStr())
 
-	// 注意是 Messages 不是 Message...
+	// 注意大部分plugin的返回结果是 Messages 不是 Message...
 	for _, v := range resp.Output.Choices[0].Messages {
 		if v.Content != nil {
 			log.Printf("%+v\n", v.Content.ToString())
